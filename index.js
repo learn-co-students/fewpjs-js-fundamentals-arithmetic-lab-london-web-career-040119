e9b1ea4e0c43c8471c499382b6ebee3fb7b0ef13
Ivan Szebenszki <szib77@gmail.com>
2019-05-19 10:15:59 +0100

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random * 42 + 1);
 
-// Write your code here
+const mod = 19 % 5;
+
+const max = Math.max(12, 4, 3, 2, 4, 20, 3);
